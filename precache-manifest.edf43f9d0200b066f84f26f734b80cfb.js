0f66335aa89264b29d35019b0956da91688a682e
Jan Carlos Charris <jancarlosch@outlook.es>
2022-06-05 00:12:48 -0500

    Updates

---
+++ precache-manifest.edf43f9d0200b066f84f26f734b80cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abacbe62a1a6682f8362",
+    "url": "/static/css/main.dbf30f8e.chunk.css"
+  },
+  {
+    "revision": "abacbe62a1a6682f8362",
+    "url": "/static/js/main.abacbe62.chunk.js"
+  },
+  {
+    "revision": "897aa20ba8584a8a9d67",
+    "url": "/static/js/1.897aa20b.chunk.js"
+  },
+  {
+    "revision": "98083e49225a7174afc4",
+    "url": "/static/js/2.98083e49.chunk.js"
+  },
+  {
+    "revision": "1b1e39ef429f9ba848b0",
+    "url": "/static/js/runtime~main.1b1e39ef.js"
+  },
+  {
+    "revision": "c26fcbd13040854640a9bfd2859f9305",
+    "url": "/index.html"
+  }
+];
